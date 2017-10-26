0c6c78a17a4bbdac079ea5ead57bd13f91bd1555
Braden Simpson <braden@uvic.ca>
2017-10-26 16:00:50 +0100

    Add favicons to tab list items

--- src/js/popup/tab_list_item.js
+++ src/js/popup/tab_list_item.js
@@ -7,6 +7,7 @@ let cx = classnames.bind(styles)
 const TabListItem = ({ tab, selected, active, onClick }) => {
   return (
     <li onClick={() => onClick(tab.id)} className={cx({ tab: true, active: tab.active, selected })}>
+      <img className={styles.favicon} src={tab.favIconUrl} />
       { tab.title }
     </li>
   )
